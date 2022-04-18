ba3d507bd7dfb9034dacbdf73c10b33ea6356aa6
Junseok <yalvz@naver.com>
2022-04-18 23:17:26 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Basics from './chapter01/Basics.svelte';
+</script>
+
+<main>
+	<Basics/>
+</main>
+
+<style>
+	
+</style>
---
+++ src/chapter01/Basics.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<div>
+  <h1>Svelte?</h1>
+  <div>
+    <p>Svelte는 빠른 웹 애플리케이션을 구축하기 위한 도구입니다.</p>
+    <p>Svelte는 런타임 에 애플리케이션 코드를 해석하지 않고 빌드 시 앱을 이상적인 JavaScript로 변환합니다 .</p>
+  </div>
+</div>
