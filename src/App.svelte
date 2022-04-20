400ce841ecd92dfd07c0871a90c283e003f28e84
Junseok <yalvz@naver.com>
2022-04-20 20:50:26 +0900

    3, 4, 5장 파일 정리

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	// import Chapter01 from './chapter01/Chapter01.svelte';
 	// import Chapter02 from './chapter02/Chapter02.svelte';
-	import Chapter03 from './chapter03/Chapter03.svelte';
+	// import Chapter03 from './chapter03/Chapter03.svelte';
+	import Chapter04 from './chapter04/Chapter04.svelte';
 </script>
 
 <main>
 	<!-- <Chapter01/> -->
 	<!-- <Chapter02/> -->
-	<Chapter03/>
+	<!-- <Chapter03/> -->
+	<Chapter04/>
 </main>
