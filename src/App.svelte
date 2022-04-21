59f4c4c340f9fe2aa21dfc07282262ecfb337936
Junseok <yalvz@naver.com>
2022-04-21 21:06:57 +0900

    chapter06 정리

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,8 @@
 	// import Chapter02 from './chapter02/Chapter02.svelte';
 	// import Chapter03 from './chapter03/Chapter03.svelte';
 	// import Chapter04 from './chapter04/Chapter04.svelte';
-	import Chapter05 from './chapter05/Chapter05.svelte';
+	// import Chapter05 from './chapter05/Chapter05.svelte';
+	import Chapter06 from './chapter06/Chapter06.svelte';
 </script>
 
 <main>
@@ -11,5 +12,6 @@
 	<!-- <Chapter02/> -->
 	<!-- <Chapter03/> -->
 	<!-- <Chapter04/> -->
-	<Chapter05/>
+	<!-- <Chapter05/> -->
+	<Chapter06/>
 </main>
---
+++ src/chapter06/Chapter06.svelte
@@ -0,0 +1,31 @@
+<script>
+  import TextInputs from './TextInputs.svelte';
+  import NumericInputs from './NumericInputs.svelte';
+  import CheckboxInputs from './CheckboxInputs.svelte';
+  import GroupInputs from './GroupInputs.svelte';
+  import TextareaInputs from './TextareaInputs.svelte';
+  import SelectBindings from './SelectBindings.svelte';
+  import SelectMultiple from './SelectMultiple.svelte';
+  import ContenteditableBindings from './ContenteditableBindings.svelte';
+  import EachBlockBindings from './TextInputs.svelte';
+  import MediaElements from './MediaElements.svelte';
+  import Dimensions from './Dimensions.svelte';
+  import This from './This.svelte';
+  import ComponentBindings from './ComponentBindings.svelte';
+  import BindingToComponentInstances from './MediaElements.svelte';
+</script>
+
+<TextInputs/>
+<NumericInputs/>
+<CheckboxInputs/>
+<GroupInputs/>
+<TextareaInputs/>
+<SelectBindings/>
+<SelectMultiple/>
+<ContenteditableBindings/>
+<EachBlockBindings/>
+<MediaElements/>
+<Dimensions/>
+<This/>
+<ComponentBindings/>
+<BindingToComponentInstances/>
