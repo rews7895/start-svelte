c9a6207b7ebb07582cdec920fd75dd2bfd9c32b2
Junseok <yalvz@naver.com>
2022-04-25 21:00:17 +0900

    chapter06 정리

--- src/chapter06/BindingToComponentInstances.svelte
+++ src/chapter06/BindingToComponentInstances.svelte
@@ -7,9 +7,9 @@
 <div>
   <h1>06-14. Binding to component instances</h1>
   <p>DOM 요소에 바인딩할 수 있는 것처럼 구성 요소 인스턴스 자체에 바인딩할 수 있습니다. </p>
-</div>
-<InputField bind:this={field} />
+  <InputField bind:this={field} />
 
-<button on:click="{() => field.focus()}">
-	Focus field
-</button>
+  <button on:click="{() => field.focus()}">
+    Focus field
+  </button>
+</div>
